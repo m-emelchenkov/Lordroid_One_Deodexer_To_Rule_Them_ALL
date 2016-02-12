d588903aef0e553d7ffd475781421232ecd68815
lord-ralf-adolf <rachidboudjelida@gmail.com>
2016-02-12 23:22:12 +0100

    add a command line implementation on the tools so the users who can't use graphical interface or cookers who wanna make scripts can still use the software

--- src/deodex/Tester.java
+++ src/deodex/Tester.java
@@ -16,14 +16,21 @@
 package deodex;
 
 import java.awt.EventQueue;
+import java.io.File;
 
+import deodex.controlers.CommandLineWorker;
+import deodex.controlers.MainWorker;
+import deodex.tools.CmdLogger;
+import deodex.tools.FilesUtils;
 import deodex.tools.Logger;
 import deodex.ui.LangFrame;
 import deodex.ui.Window;
 
 public class Tester {
+	public static CmdLogger logger = new CmdLogger();
+	public static CommandLineWorker rootWorker = new CommandLineWorker();
 	public static void main(String args[]) {
-
+		if (args == null || args.length == 0){
 		if (Cfg.isFirstLaunch()) {
 			Cfg.setCurrentLang(S.ENGLISH);
 			R.initResources();
@@ -47,5 +54,95 @@ public class Tester {
 			Window win = new Window();
 		}
 
+	} else if (args.length > 3){
+		printHelp();
+	}  else if (args.length == 1 && args[0].equals("-h") ){
+		R.initResources();
+		printHelp();
+	} else {
+				int lengh = args.length;
+				// the user used one argument and it's not '-h' so we assume he selected a folder 
+				R.initResources();
+				File systemFolder = new File(args[0]);
+				boolean sign = false;
+				boolean zipAlign = false;
+				if (lengh == 2){
+				if (args[1].equals("-z") ||args[1].equals("-s") ){
+					if(args[1].equals("-z")){
+						zipAlign = true; 
+					}
+					if(args[1].equals("-s")){
+						sign = true;
+					}
+				}else {
+						System.out.println("unkown option !");
+						printHelp();
+						return;
+					}
+				} else {
+					if (args[1].equals("-z") || args[1].equals("-s") ){
+
+					if(args[1].equals("-z")){
+						zipAlign = true; 
+					} 
+					if(args[1].equals("-s")){
+						sign = true;
+					} 
+					}else {
+						System.out.println("unkown option !");
+						printHelp();
+						return;
+					}
+					if (args[2].equals("-z") ||args[2].equals("-s") ){
+
+					if(args[2].equals("-z")){
+						zipAlign = true; 
+					} 
+					if(args[2].equals("-s")){
+						sign = true;
+					}
+					} else {
+						System.out.println("unkown option !");
+						printHelp();
+						return;
+					}
+				}
+				if(!systemFolder.exists() ){
+					logger.addLog(systemFolder.getAbsolutePath()+" no such folder double check the path !");
+					printHelp();
+					return;
+				} else if(!systemFolder.isDirectory()){
+					logger.addLog(systemFolder.getAbsolutePath()+" is not a directory !");
+					printHelp();
+					return;
+				} else if(FilesUtils.isAValideSystemDir(systemFolder, logger)){
+					proseedWithNoGui(systemFolder , sign ,zipAlign);
+				}
+			}
+	}
+	
+	
+	private static void proseedWithNoGui(File systemFolder ,boolean sign ,boolean zipalign){
+		SessionCfg.sign = sign;
+		SessionCfg.setSign(sign);
+		SessionCfg.zipalign = zipalign;
+		SessionCfg.setZipalign(zipalign);
+		MainWorker mainWorker = new MainWorker(systemFolder, logger, 1);
+		mainWorker.addThreadWatcher(new CommandLineWorker());
+		Thread t = new Thread(mainWorker);
+		
+		t.start();
+	}
+	
+	
+	private static void printHelp(){
+		
+		System.out.println("Lordroid batch deodex :\n");
+		System.out.println("USAGE :\n");
+		System.out.println("java -jar lordroid-ODTRTA.jar <systemFolder> [OPTIONS]");
+		System.out.println("Options");
+		System.out.println("-z : zipalign every apk after deodexing it");
+		System.out.println("-s sign every apk after deodexing");
+		System.out.println("-h : print this help page");
 	}
 }
---
+++ src/deodex/tools/CmdLogger.java
@@ -0,0 +1,52 @@
+/*
+ * 
+ * 
+ * Copyright 2016 Rachid Boudjelida <rachidboudjelida@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package deodex.tools;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import deodex.controlers.LoggerPan;
+
+public class CmdLogger implements LoggerPan {
+	ArrayList<String> logs = new ArrayList<String>();
+	
+	@Override
+	public synchronized void addLog(String str) {
+		// TODO Auto-generated method stub
+		long yourmilliseconds = System.currentTimeMillis();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss"); // dd/MMM/yyyy
+		Date resultdate = new Date(yourmilliseconds);
+		String str2 = sdf.format(resultdate);
+		logs.add("["+str2+"]"+str);
+		System.out.println("["+str2+"]"+str);
+	}
+
+	@Override
+	public synchronized void saveToFile() {
+		// TODO Auto-generated method stub
+		long yourmilliseconds = System.currentTimeMillis();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss"); // dd/MMM/yyyy
+		Date resultdate = new Date(yourmilliseconds);
+		String str = PathUtils.getExcutionPath() + File.separator + "logs" + File.separator + sdf.format(resultdate)
+				+ ".log";
+		File logFile = new File(str);
+		this.addLog("Log file saved to "+logFile.getAbsolutePath());
+		PropReader.ArrayToProp(logs, logFile);
+	}
+
+}
